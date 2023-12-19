30a0b24fe362d4495cfebe369f4fadefa4f11f08
leeyuan <dbdks32123@gmail.com>
2023-12-19 19:30:16 +0900

    nest - nest - req, res

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,3 +1,4 @@
+// controller
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
@@ -6,7 +7,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    const result = await this.appService.getHello();
+    return result;
   }
 }
