214b5933a99459bec370abc7ae169787d28050e6
leeyuan <dbdks32123@gmail.com>
2023-12-19 18:13:56 +0900

    main.ts - CORS del

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors(); // CORS 활성화
-
-  await app.listen(3001);
+  await app.listen(3000);
 }
 bootstrap();
