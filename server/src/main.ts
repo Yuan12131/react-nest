4a3af0b8e2332a715688740a7ce3d5365c4b827f
leeyuan <dbdks32123@gmail.com>
2023-12-19 19:29:30 +0900

    nest - 연결 완료 메세지 출력

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
+// main
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  // MariaDB 연결이 성공하면 '완료' 출력
+  console.log('완료');
 }
 bootstrap();
