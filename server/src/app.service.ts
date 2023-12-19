bdc1b0ffb6a37eb835f743a815bab24e4a732f78
leeyuan <dbdks32123@gmail.com>
2023-12-19 19:29:40 +0900

    nest - 연결 완료 메세지 출력

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,20 @@
+// service
 import { Injectable } from '@nestjs/common';
+import { Connection } from 'typeorm';
+import { InjectConnection } from '@nestjs/typeorm';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
+  async getHello(): Promise<string> {
+    try {
+      // TypeOrmModule을 사용하여 MariaDB 연결 확인
+      await this.connection.query('SELECT 1');
+      return '완료';
+    } catch (error) {
+      console.error('MariaDB 연결 실패:', error.message);
+      return 'MariaDB 연결 실패';
+    }
   }
 }
