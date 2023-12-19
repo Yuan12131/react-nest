d267d9af52203f83bad09ae4727aa6c518383869
leeyuan <dbdks32123@gmail.com>
2023-12-19 19:30:37 +0900

    nest - typeORM config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,22 @@
+// module
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'nest',
+      password: '1234',
+      database: 'nest',
+      synchronize: true,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
